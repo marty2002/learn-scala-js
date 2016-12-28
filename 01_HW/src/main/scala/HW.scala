445c4cd0f834f505414e8263540545cb2059c38a
maria <maria@eldis.ru>
2016-12-28 09:58:43 +0700

    a simple console HW-application

---
+++ 01_HW/src/main/scala/HW.scala
@@ -0,0 +1,11 @@
+import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import org.scalajs.dom.document
+
+package hw {
+	object HelloWorldApp extends JSApp {
+		def main(): Unit = {
+			println("Hello world!")
+		}
+	}
+}
