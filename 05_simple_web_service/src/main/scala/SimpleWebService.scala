2977c7cc1e372aed42e7494e435812d5d3d15c75
maria <maria@eldis.ru>
2017-01-16 10:10:03 +0700

    simple web service app example

--- 05_simple_web_service/src/main/scala/SimpleWebService.scala
+++ 05_simple_web_service/src/main/scala/SimpleWebService.scala
@@ -2,24 +2,42 @@ import scala.scalajs.js.JSApp
 import scalatags.JsDom.all._
 import org.scalajs.dom
 import dom.document
+import scalajs.js
 
 package tutorial {
-	object SimpleWebService extends JSApp {
+
+  import scala.scalajs.js
+
+  object SimpleWebService extends JSApp {
 		def main(): Unit = {
       import dom.ext._
-      import scala.scalajs
-        .concurrent
-        .JSExecutionContext
-        .Implicits.queue
+      import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue // для использования асинхронных запросов
 
       val url =
         "https://jsonplaceholder.typicode.com" +
           "/posts/1"
 
       Ajax.get(url).onSuccess{ case xhr =>
-        dom.document.getElementById("app").appendChild(
-          pre(xhr.responseText).render
-        )
+        /*dom.document.getElementById("app").appendChild(
+          pre(
+            js.JSON.stringify(
+              js.JSON.parse(xhr.responseText),
+              space=4
+            )
+          ).render
+        )*/
+        if (xhr.status == 200) {
+          val json = js.JSON.parse(xhr.responseText)
+          val title = json.title.toString
+          val body = json.body.toString
+          dom.document.getElementById("app").appendChild(
+              div(
+                h1(title).render,
+                div(body).render,
+                pre(js.JSON.stringify( json, space=4)).render
+              ).render
+          ).render
+        }
       }
 		}
 	}
